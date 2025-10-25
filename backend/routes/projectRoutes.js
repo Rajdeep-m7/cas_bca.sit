925b6c5dde4a70c28a0d54d2dd56e2512517dd6e
Supratik Ray <raySupratik8@gmail.com>
2025-10-25 19:23:33 +0530

    added all the logic for projectController

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -6,6 +6,7 @@ import {
   createProject,
   updateProject,
   deleteProject,
+  getProject,
 } from "../controllers/projectController.js";
 import {
   createComment,
@@ -17,7 +18,11 @@ import { createLike, deleteLike } from "../controllers/likeController.js";
 import { uploadImage } from "../controllers/uploadController.js";
 
 //validators
-import { createProjectValidator } from "../validators/projectValidator.js";
+import {
+  createProjectValidator,
+  updateProjectValidator,
+} from "../validators/projectValidator.js";
+import { mongoIdValidator } from "../validators/mongoValidator.js";
 import validateRequest from "../middlewares/validateRequest.js";
 
 //middlewares
@@ -33,7 +38,15 @@ router.use(authenticationMiddleware);
 //projects
 router.route("/").post(createProjectValidator, validateRequest, createProject);
 
-router.route("/:id").patch(updateProject).delete(deleteProject);
+router
+  .route("/:id")
+  .get(mongoIdValidator, validateRequest, getProject)
+  .patch(
+    [mongoIdValidator, updateProjectValidator],
+    validateRequest,
+    updateProject
+  )
+  .delete(mongoIdValidator, validateRequest, deleteProject);
 
 //comments
 router.route("/:id/comments").get(getAllComments).post(createComment);
